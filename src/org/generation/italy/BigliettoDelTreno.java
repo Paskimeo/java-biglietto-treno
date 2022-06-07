bf743d403eec0290a4ea26674b667979ab2c0c1d
paskimeo <paskimeo@gmail.com>
2022-06-07 14:57:57 +0200

    aggiunto DecimalFormat

--- src/org/generation/italy/BigliettoDelTreno.java
+++ src/org/generation/italy/BigliettoDelTreno.java
@@ -1,14 +1,19 @@
 package org.generation.italy;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
 
 public class BigliettoDelTreno {
 
 	public static void main(String[] args) {
 		
+		
+		
 		    int eta;
 		    int kilometri;
 		    Scanner scan = new Scanner(System.in);
+		    DecimalFormat df = new DecimalFormat(".00");
 
 		    System.out.print("Inserisci la tua età: ");
 		    eta = scan.nextInt();
@@ -20,15 +25,15 @@ public class BigliettoDelTreno {
 		    System.out.println("kilometri scelti: " + kilometri);
 
 		    float costoBiglietto = kilometri * 0.21f;
-		    System.out.println("costo del biglietto: " + costoBiglietto);
+		    System.out.println("costo del biglietto: " + df.format(costoBiglietto));
 		    
 		    if(eta < 18) {
 		    	float costoBigliettoGiovani = costoBiglietto - costoBiglietto * 20 / 100;
-		    	System.out.println("prezzo con lo sconto young: " + costoBigliettoGiovani);
+		    	System.out.println("prezzo con lo sconto young: " + df.format(costoBigliettoGiovani));
 		      
 		    }else if (eta > 65) {
 		    	float costoBigliettoAnziani = costoBiglietto - costoBiglietto * 40 / 100;
-		    	System.out.println("prezzo con lo sconto young: " + costoBigliettoAnziani);
+		    	System.out.println("prezzo con lo sconto young: " + df.format(costoBigliettoAnziani));
 		    }
 		    
 		    
